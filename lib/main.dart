b00ca2173acca73239973bcfaf379961f703bab5
Thomas Jeffrey <sgthomasejeffrey@gmail.com>
2020-12-05 04:41:29 +0000

    new push

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-
-//import 'package:flutter_auth/Screens/Welcome/welcome_screen.dart';
 import 'package:flutter_auth/Screens/Login/login_screen.dart';
 import 'package:flutter_auth/constants.dart';
 import 'package:sizer/sizer.dart';
